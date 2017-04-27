18c3d1156df6e42cdb952fa66af0e6eb408d9bc1
cuylerwarnock <cuyler.warnock@bobcats.gcsu.edu>
2017-04-26 22:05:33 -0400

    vehicle update

--- Node/public/js/vehicles.js
+++ Node/public/js/vehicles.js
@@ -1,5 +1,13 @@
 angular.module("mainApp").controller("vehiclesController", function($scope, $stateParams){
 	$scope.vehiclesArr = [];
+	$scope.form = {};
 
-	
+	$scope.addVehicle = function(vehicle) {
+		var newVehicle = {
+			year: form.year,
+			make: form.make,
+			model: form.model,
+			history: []
+		};
+	}
 })
